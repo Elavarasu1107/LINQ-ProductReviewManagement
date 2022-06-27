d3d930ad9862af077248f7ffe2dd4cd274e369f4
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-27 12:18:04 +0530

    [Elavarasu] UC1-Creating Product class and Adding Values

---
+++ ProductReview/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReview
+{
+    public class ProductModel
+    {
+        public int productID { get; set; }
+        public int userID { get; set; }
+        public double rating { get; set; }
+        public string review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
